303992f0da6ed1ba135f8d669af4b9acfe4d6a5e
Galina <shaman282861@gmail.com>
2019-10-23 09:35:01 +0200

    scroll

--- src/main.js
+++ src/main.js
@@ -8,3 +8,30 @@ import "./scripts/parallax";
 import "./scripts/budda__parallax";
 import "./scripts/works";
 import "./scripts/reviews";
+import Menu from "./scripts/menu";
+
+import Vue from 'vue';
+import VueScrollTo from 'vue-scrollto';
+
+
+
+
+Vue.use(VueScrollTo, {
+     container: "body",
+     duration: 500,
+     easing: "ease",
+     offset: 0,
+     force: true,
+     cancelable: true,
+     onStart: false,
+     onDone: false,
+     onCancel: false,
+     x: false,
+     y: true
+ })
+new Vue ({
+  el: '.header__menu',
+  components: {
+  vcMenu: Menu
+  }
+});
